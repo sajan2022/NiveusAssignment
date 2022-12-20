d9759957d07a2be2a821c911fb7c5ca4a996d089
sajan2022 <sajan.bappal@niveussolutions.com>
2022-12-20 15:18:28 +0530

    sql query assignment2

---
+++ Queries/queries.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO employee (emp_name,department) VALUES('sajan','IT');
+
+UPDATE employee set name = 'sajan b' WHERE emp_id = 1;
+
+SELECT * FROM  employee;
+
+DELETE FROM employee WHERE emp_id = 1;
